8f836fb2907b1a7241d09c27ec94d0de7508b4c1
anonymous429 <984010938@qq.com>
2021-03-27 13:20:41 +0800

    Add output info

--- gnnrl_network_pruning.py
+++ gnnrl_network_pruning.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-from copy import deepcopy
 
 from torch import nn
 from torchvision import models
@@ -14,20 +13,13 @@ from lib.RL.agent import Memory, Agent
 
 logging.disable(30)
 from graph_env.graph_environment import graph_env
-from utils.split_dataset import get_split_valset_ImageNet, get_split_train_valset_CIFAR, \
-    get_split_train_valset_CIFAR100, get_dataset
+from utils.split_dataset import get_split_valset_ImageNet, get_split_train_valset_CIFAR, get_dataset
 
 torch.backends.cudnn.deterministic = True
-# from lib.agent import DDPG
-from utils.train_utils import get_output_folder, to_numpy, to_tensor, plot_learning_curve
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='gnnrl search script')
-    #parser.add_argument()
-    parser.add_argument('--job', default='train', type=str, help='support option: train/export')
-    parser.add_argument('--suffix', default=None, type=str, help='suffix to help you remember what experiment you ran')
-    #graph encoder
 
     # datasets and model
     parser.add_argument('--model', default='mobilenet', type=str, help='model to prune')
@@ -48,8 +40,6 @@ def parse_args():
 
     parser.add_argument('--compression_ratio', default=0.5, type=float,
                         help='compression_ratio')
-    # parser.add_argument('--pruning_method', default='cp', type=str,
-    #                     help='method to prune (fg/cp/cpfg for fine-grained and channel pruning)')
     parser.add_argument('--n_calibration_batches', default=60, type=int,
                         help='n_calibration_batches')
     parser.add_argument('--n_points_per_layer', default=10, type=int,
@@ -63,57 +53,38 @@ def parse_args():
     parser.add_argument('--g_hidden_size', default=50, type=int, help='hidden num of second fully connect layer')
     parser.add_argument('--g_embedding_size', default=50, type=int, help='hidden num of second fully connect layer')
     parser.add_argument('--hidden_size', default=300, type=int, help='hidden num of first fully connect layer')
+    parser.add_argument('--solved_reward', default=0, type=int, help='stop training if avg_reward > solved_reward')
     parser.add_argument('--log_interval', default=20, type=int, help='print avg reward in the interval')
     parser.add_argument('--max_episodes', default=10000, type=int, help='max training episodes')
     parser.add_argument('--max_timesteps', default=1500, type=int, help='max timesteps in one episode')
-    parser.add_argument('--update_timestep', default=50, type=int, help='update policy every n timesteps')
+    parser.add_argument('--update_timestep', default=100, type=int, help='update policy every n timesteps')
     parser.add_argument('--action_std', default=0.5, type=float, help='constant std for action distribution (Multivariate Normal)')
-    parser.add_argument('--K_epochs', default=80, type=int, help='update policy for K epochs')
+    parser.add_argument('--K_epochs', default=20, type=int, help='update policy for K epochs')
     parser.add_argument('--eps_clip', default=0.2, type=float, help='clip parameter for RL')
     parser.add_argument('--gamma', default=0.99, type=float, help='discount factor')
     parser.add_argument('--lr', default=0.0003, type=float, help='learning rate for optimizer')
 
-    # parser.add_argument('--lr_c', default=1e-3, type=float, help='learning rate for actor')
-    # parser.add_argument('--lr_a', default=1e-3, type=float, help='learning rate for actor')
-    # parser.add_argument('--warmup', default=25, type=int,
-    #                     help='time without training but only filling the replay memory')
-    # parser.add_argument('--discount', default=1., type=float, help='')
-    # parser.add_argument('--bsize', default=64, type=int, help='minibatch size')
-    # parser.add_argument('--rmsize', default=100, type=int, help='memory size for each layer')
-    # parser.add_argument('--window_length', default=1, type=int, help='')
-    # parser.add_argument('--tau', default=0.01, type=float, help='moving average for target network')
-    # # noise (truncated normal distribution)
-    # parser.add_argument('--init_delta', default=0.5, type=float,
-    #                     help='initial variance of truncated normal distribution')
-    # parser.add_argument('--delta_decay', default=0.99, type=float,
-    #                     help='delta decay during exploration')
-    # training
-    parser.add_argument('--disable', default=None, type=str, help='cuda/cpu')
     parser.add_argument('--device', default='cuda', type=str, help='cuda/cpu')
-    parser.add_argument('--max_episode_length', default=1e9, type=int, help='')
     parser.add_argument('--output', default='./logs', type=str, help='')
     parser.add_argument('--debug', dest='debug', action='store_true')
     parser.add_argument('--init_w', default=0.003, type=float, help='')
-    parser.add_argument('--train_episode', default=800, type=int, help='train iters each timestep')
-    parser.add_argument('--epsilon', default=50000, type=int, help='linear decay of exploration policy')
     parser.add_argument('--seed', default=None, type=int, help='random seed to set')
     parser.add_argument('--n_gpu', default=1, type=int, help='number of gpu to use')
-    parser.add_argument('--n_worker', default=16, type=int, help='number of data loader worker')
-    parser.add_argument('--data_bsize', default=50, type=int, help='number of data batch size')
-    parser.add_argument('--resume', default='default', type=str, help='Resuming model path for testing')
-    # export
+    parser.add_argument('--n_worker', default=32, type=int, help='number of data loader worker')
+    parser.add_argument('--data_bsize', default=256, type=int, help='number of data batch size')
+
 
     parser.add_argument('--log_dir', default='./logs', type=str, help='log dir')
     parser.add_argument('--ratios', default=None, type=str, help='ratios for pruning')
-    parser.add_argument('--channels', default=None, type=str, help='channels after pruning')
-    parser.add_argument('--export_path', default=None, type=str, help='path for exporting models')
-    parser.add_argument('--use_new_input', dest='use_new_input', action='store_true', help='use new input feature')
+    # parser.add_argument('--channels', default=None, type=str, help='channels after pruning')
+    # parser.add_argument('--export_path', default=None, type=str, help='path for exporting models')
+    # parser.add_argument('--use_new_input', dest='use_new_input', action='store_true', help='use new input feature')
 
     return parser.parse_args()
 
 def search(env):
     ############## Hyperparameters ##############
-    env_name = "BipedalWalker-v2"
+    env_name = "gnnrl_search"
     render = False
     solved_reward = args.solved_reward         # stop training if avg_reward > solved_reward
     log_interval = args.log_interval           # print avg reward in the interval
@@ -129,7 +100,7 @@ def search(env):
     lr = args.lr                 # parameters for Adam optimizer
     betas = (0.9, 0.999)
 
-    random_seed = None
+    random_seed = args.seed
     #############################################
 
     state_dim = args.g_in_size
@@ -142,13 +113,14 @@ def search(env):
 
     memory = Memory()
     agent = Agent(state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
-    print(lr,betas)
+    print("Learning rate: ",lr,'\t Betas: ',betas)
 
     # logging variables
     running_reward = 0
     avg_length = 0
     time_step = 0
 
+    print("-*"*10,"start search the pruning policies","-*"*10)
     # training loop
     for i_episode in range(1, max_episodes+1):
         state = env.reset()
@@ -164,9 +136,12 @@ def search(env):
 
             # update if its time
             if time_step % update_timestep == 0:
+                print("-*"*10,"start training the RL agent","-*"*10)
                 agent.update(memory)
                 memory.clear_memory()
                 time_step = 0
+                print("-*"*10,"start search the pruning policies","-*"*10)
+
             running_reward += reward
             if render:
                 env.render()
@@ -176,14 +151,14 @@ def search(env):
         avg_length += t
 
         # stop training if avg_reward > solved_reward
-        if running_reward > (log_interval*solved_reward):
+        if (i_episode % log_interval)!=0 and running_reward/(i_episode % log_interval) > (solved_reward):
             print("########## Solved! ##########")
-            torch.save(agent.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format(env_name))
+            torch.save(agent.policy.state_dict(), './rl_solved_{}.pth'.format(env_name))
             break
 
         # save every 500 episodes
         if i_episode % 500 == 0:
-            torch.save(agent.policy.state_dict(), './PPO_continuous_{}.pth'.format(env_name))
+            torch.save(agent.policy.state_dict(), './rl_{}.pth'.format(env_name))
 
         # logging
         if i_episode % log_interval == 0:
@@ -195,58 +170,6 @@ def search(env):
             avg_length = 0
 
 
-def train(agent, env, output,args):
-
-    agent.is_training = True
-    step = episode = episode_steps = 0
-    episode_reward = 0.
-    observation = None
-    T = []  # trajectory
-    while episode < args.train_episode:  # counting based on episode
-        # reset if it is the start of episode
-        if observation is None:
-            observation = env.reset()
-            agent.reset(observation)
-
-        # agent pick action ...
-        if episode <= args.warmup:
-            action = agent.random_action()
-        else:
-            action = agent.select_action(observation, episode=episode)
-        #print(action)
-        # env response with next_observation, reward, terminate_info
-        observation2, reward, done = env.step(action)
-        #observation2 = (observation2)
-
-
-        T.append([reward, observation, observation2, action, done])
-
-        if episode % int(args.train_episode / 3) == 0:
-            agent.save_model(output)
-
-        # update
-        step += 1
-        episode_steps += 1
-        episode_reward += reward
-        observation = observation2
-
-        if done:  # end of episode
-            print("Search Episode: ",episode)
-
-
-            final_reward = T[-1][0]
-            print('final_reward: {}\n'.format(final_reward))
-            # agent observe and update policy
-            for r_t, s_t, s_t1, a_t, done in T:
-                agent.observe(final_reward, s_t, s_t1, a_t, done)
-                if episode > args.warmup:
-                    agent.update_policy()
-            observation = None
-            episode_steps = 0
-            episode_reward = 0.
-            episode += 1
-            T = []
-            print('-' * 40)
 
 
 def load_model(model_name,data_root,device=None):
@@ -349,10 +272,7 @@ def get_prunable_idx(net,args):
 
 def get_num_hidden_layer(net,args):
     layer_share=0
-    if args.pruning_method == "cp":
-        prunable_layer_types = [torch.nn.Conv2d]
-    else:
-        prunable_layer_types = [torch.nn.Conv2d, torch.nn.Linear]
+
     n_layer=0
 
     if args.model in ['mobilenet']:
@@ -406,7 +326,7 @@ if __name__ == "__main__":
     if args.dataset == "ILSVRC":
         path = args.data_root
 
-        train_loader, val_loader, n_class = get_split_valset_ImageNet("ILSVRC", 128, 4, args.train_size, args.val_size,
+        train_loader, val_loader, n_class = get_split_valset_ImageNet("ILSVRC", args.data_bsize, 4, args.train_size, args.val_size,
                                                                       data_root=path,
                                                                       use_real_val=True, shuffle=True)
         input_x = torch.randn([1,3,224,224]).to(device)
@@ -414,16 +334,13 @@ if __name__ == "__main__":
     elif args.dataset == "cifar10":
         path = os.path.join(args.data_root, "datasets")
 
-        train_loader, val_loader, n_class = get_split_train_valset_CIFAR('cifar10', 256, 4, args.train_size, args.val_size,
+        train_loader, val_loader, n_class = get_split_train_valset_CIFAR(args.dataset, args.data_bsize, 4, args.train_size, args.val_size,
                                                                          data_root=path, use_real_val=False,
                                                                          shuffle=True)
         input_x = torch.randn([1,3,32,32]).to(device)
     elif args.dataset == "cifar100":
         path = os.path.join(args.data_root, "datasets")
 
-        # train_loader, val_loader, n_class = get_split_train_valset_CIFAR100('cifar100', 256, 4, args.train_size, args.val_size,
-        #                                                                  data_root=path, use_real_val=True,
-        #                                                                  shuffle=True)
         train_loader, val_loader, n_class = get_dataset(args.dataset, 256, args.n_worker,
                                                         data_root=args.data_root)
         input_x = torch.randn([1,3,32,32]).to(device)
@@ -433,16 +350,7 @@ if __name__ == "__main__":
 
 
     env = graph_env(net,n_layer,args.dataset,val_loader,args.compression_ratio,args.g_in_size,args.log_dir,input_x,device,args)
-    # hierarchical_graph = env.model_to_graph()
-    # agent = Agent(layer_share,args.g_in_size)
-    # agent.cuda()
-    #agent.load_weights(args.log_dir)
-    #env._set_graph_encoder(agent.graph_encoder)
-    # if args.disable == "graph_encoder":
-    #     agent.graph_grad_status(False)
-    #
     search(env)
-    # search(agent, env, args.output, args)
 
 #python -W ignore gnnrl_network_pruning.py --dataset cifar10 --model resnet110 --compression_ratio 0.4 --log_dir ./logs
 #python -W ignore gnnrl_network_pruning.py --lr_c 0.01 --lr_a 0.01 --dataset cifar100 --bsize 32 --model shufflenetv2 --compression_ratio 0.2 --warmup 100 --pruning_method cp --val_size 1000 --train_episode 300 --log_dir ./logs
--- lib/RL/agent.py
+++ lib/RL/agent.py
@@ -185,7 +185,7 @@ class Agent:
             surr1 = ratios * advantages
             surr2 = torch.clamp(ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
             loss = -torch.min(surr1, surr2) + 0.5*self.MseLoss(state_values, rewards) - 0.01*dist_entropy
-            
+            print('Epoches {} \t loss: {} \t '.format(_, loss.mean()))
             # take gradient step
             self.optimizer.zero_grad()
             loss.mean().backward()
@@ -194,92 +194,7 @@ class Agent:
         # Copy new weights into old policy:
         self.policy_old.load_state_dict(self.policy.state_dict())
         
-def main():
-    ############## Hyperparameters ##############
-    env_name = "BipedalWalker-v3"
-    render = False
-    solved_reward = 300         # stop training if avg_reward > solved_reward
-    log_interval = 20           # print avg reward in the interval
-    max_episodes = 10000        # max training episodes
-    max_timesteps = 1500        # max timesteps in one episode
-    
-    update_timestep = 4000      # update policy every n timesteps
-    action_std = 0.5            # constant std for action distribution (Multivariate Normal)
-    K_epochs = 80               # update policy for K epochs
-    eps_clip = 0.2              # clip parameter for RL
-    gamma = 0.99                # discount factor
-    
-    lr = 0.0003                 # parameters for Adam optimizer
-    betas = (0.9, 0.999)
-    
-    random_seed = None
-    #############################################
-    
-    # creating environment
-    env = gym.make(env_name)
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
-    
-    if random_seed:
-        print("Random Seed: {}".format(random_seed))
-        torch.manual_seed(random_seed)
-        env.seed(random_seed)
-        np.random.seed(random_seed)
-    
-    memory = Memory()
-    ppo = Agent(state_dim, action_dim, action_std, lr, betas, gamma, K_epochs, eps_clip)
-    print(lr,betas)
-    
-    # logging variables
-    running_reward = 0
-    avg_length = 0
-    time_step = 0
-    
-    # training loop
-    for i_episode in range(1, max_episodes+1):
-        state = env.reset()
-        for t in range(max_timesteps):
-            time_step +=1
-            # Running policy_old:
-            action = ppo.select_action(state, memory)
-            state, reward, done, _ = env.step(action)
+
             
-            # Saving reward and is_terminals:
-            memory.rewards.append(reward)
-            memory.is_terminals.append(done)
-            
-            # update if its time
-            if time_step % update_timestep == 0:
-                ppo.update(memory)
-                memory.clear_memory()
-                time_step = 0
-            running_reward += reward
-            if render:
-                env.render()
-            if done:
-                break
-        
-        avg_length += t
-        
-        # stop training if avg_reward > solved_reward
-        if running_reward > (log_interval*solved_reward):
-            print("########## Solved! ##########")
-            torch.save(ppo.policy.state_dict(), './PPO_continuous_solved_{}.pth'.format(env_name))
-            break
-        
-        # save every 500 episodes
-        if i_episode % 500 == 0:
-            torch.save(ppo.policy.state_dict(), './PPO_continuous_{}.pth'.format(env_name))
-            
-        # logging
-        if i_episode % log_interval == 0:
-            avg_length = int(avg_length/log_interval)
-            running_reward = int((running_reward/log_interval))
-            
-            print('Episode {} \t Avg length: {} \t Avg reward: {}'.format(i_episode, avg_length, running_reward))
-            running_reward = 0
-            avg_length = 0
-            
-if __name__ == '__main__':
-    main()
+
     
